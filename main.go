f5f46d1d65ba1349b87100078e9e2e91eaba8881
Ellie <149360297+EWinterhalter@users.noreply.github.com>
2025-02-26 11:50:02 +0300

    f1

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Single Responsibility Principle (SRP) Каждый класс должен иметь только одну ответственность.
+
+type Student struct {
+	ID    int
+	Name  string
+	Grade int
+}
+
+// Open/Closed Principle (OCP) Классы должны быть открыты для расширения, но закрыты для модификации.
+
+type Printer interface {
+	Print(student Student)
+}
+
+type ConsolePrinter struct{}
+
+func (cp ConsolePrinter) Print(student Student) {
+	fmt.Printf("Student ID: %d, Name: %s, Grade: %d\n", student.ID, student.Name, student.Grade)
+}
+
+// Dependency Inversion Principle (DIP) Модули верхнего уровня не должны зависеть от модулей нижнего уровня. Оба типа модулей должны зависеть от абстракций.
+
+type StudentService struct {
+	printer Printer
+}
+
+func (ss StudentService) PrintStudentInfo(student Student) {
+	ss.printer.Print(student)
+}
+
+func main() {
+	student := Student{ID: 1, Name: "John Doe", Grade: 95}
+	printer := ConsolePrinter{}
+	studentService := StudentService{printer: printer}
+
+	studentService.PrintStudentInfo(student)
+}
